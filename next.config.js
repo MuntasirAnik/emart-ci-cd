485c9c13910a406a50b1f4b9e05e7a07d321392d
MuntasirAnik <anik.muntasir005@gmail.com>
2023-10-01 12:48:23 +0600

    add env and next config for next auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "link.fobshanghai.com",
+      "cdn.sanity.io",
+      "platform-lookaside.fbsbx.com",
+      "i.ibb.co",
+      "cdn.shopify.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
